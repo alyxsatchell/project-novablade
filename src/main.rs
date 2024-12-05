9ceb1f8e7a52940508fb46afc2a29cd303e7b7f1
alyxsatchell <alyxsatchell@gmail.com>
2024-12-04 19:58:27 -0500

    added simple setup functon that spawns a point at the origin

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 pub mod playership;
 
-use crystal_nova::universe::Universe;
+use crystal_nova::{particle::Particle, physics::Vector, universe::Universe};
 use winit::{event_loop::EventLoop, window::WindowBuilder};
 use crate::playership::PlayerShip;
 
 fn main() {
-    println!("Hello, world!");
     pollster::block_on(run());
 }
 
@@ -14,5 +13,13 @@ async fn run(){
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
     let mut universe = Universe::new(&window, Box::new(player)).await;
-    universe.run(event_loop).await;
+    let setup = |uni: &mut Universe| {
+        let (phys_col, queue) = uni.get_physics_collection_mut();
+        let particle = Particle{
+            position: Vector::new(),
+            velocity: Vector::new(),
+        };
+        phys_col.add_particle(particle, queue);
+    };
+    universe.run(event_loop, Some(Box::new(setup))).await;
 }
